62c3362b1d9f31e577bf4ac3a6900b680dc6236d
DhruvBhagadia <dhruvbhagadia@gmail.com>
2018-12-30 14:19:43 +0530

    updated Readme.md

--- Day-4/Main.java
+++ Day-4/Main.java
@@ -115,16 +115,45 @@ class LinkedList {
 		delete(nodeToBeDeleted);
 	}
 
+	void mergeTwoLLAtAlternatePositions(Node root1, int count1, Node root2, int count2){
+		Node current1 = root1;
+		Node current2 = root2;
+		int itr = 0;
+		while(current1 != null && current2 != null){
+			if(itr++%2 == 0){
+				this.insert(current1.data);
+				current1 = current1.next;
+			}
+			else{
+				this.insert(current2.data);
+				current2 = current2.next;
+			}
+		}
+		Node last = this.root;
+		while(last.next != null)
+			last = last.next;
+		if(current1 == null)
+			last.next = current2;
+		else if(current2 == null)
+			last.next = current1;
+	}
+
 }
 
 class Main {
 	public static void main(String args[]) {
 		LinkedList ll = new LinkedList();
-		int[] arr = {1, 2, 3, 1};
+		int[] arr = {1, 2, 3, 4};
 		for (int i=0; i<arr.length; i++) {
 			ll.insert(arr[i]);
 		}
-		ll.deleteLastOccurrence(1);
-		ll.display();
+		LinkedList ll2 = new LinkedList();
+		int[] arr2 = {5, 6, 8, 9, 10, 11, 12};
+		for (int i=0; i<arr2.length; i++) {
+			ll2.insert(arr2[i]);
+		}
+		LinkedList ll3 = new LinkedList();
+		ll3.mergeTwoLLAtAlternatePositions(ll.root, ll.getCount(), ll2.root, ll2.getCount());
+		ll3.display();
 	}
 }
--- Day-4/Readme.md
+++ Day-4/Readme.md
@@ -1,4 +1,4 @@
-Day2: 
+Day4: 
 1. nth node from last: https://practice.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1
 2. palindrome: https://www.geeksforgeeks.org/function-to-check-if-a-singly-linked-list-is-palindrome/
 3. delete last occurrence: http://quiz.geeksforgeeks.org/delete-last-occurrence-of-an-item-from-linked-list/
